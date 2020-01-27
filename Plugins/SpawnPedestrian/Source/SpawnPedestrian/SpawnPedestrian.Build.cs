cfe76fa65d50e62adabef475670efba6f9eb2410
Ivan Xu <info@chivalrystudio.com>
2020-01-27 13:33:20 -0800

    [fmt] run cleanup

--- Plugins/SpawnPedestrian/Source/SpawnPedestrian/SpawnPedestrian.Build.cs
+++ Plugins/SpawnPedestrian/Source/SpawnPedestrian/SpawnPedestrian.Build.cs
@@ -4,57 +4,59 @@ using UnrealBuildTool;
 
 public class SpawnPedestrian : ModuleRules
 {
-	public SpawnPedestrian(ReadOnlyTargetRules Target) : base(Target)
-	{
-		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
-		
-		PublicIncludePaths.AddRange(
-			new string[] {
-				// ... add public include paths required here ...
-			}
-			);
-				
-		
-		PrivateIncludePaths.AddRange(
-			new string[] {
-				// ... add other private include paths required here ...
-			}
-			);
-			
-		
-		PublicDependencyModuleNames.AddRange(
-			new string[]
-			{
-				"Core",
-				// ... add other public dependencies that you statically link with here ...
+    public SpawnPedestrian(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicIncludePaths.AddRange(
+            new string[]
+            {
+                // ... add public include paths required here ...
+            }
+        );
+
+
+        PrivateIncludePaths.AddRange(
+            new string[]
+            {
+                // ... add other private include paths required here ...
+            }
+        );
+
+
+        PublicDependencyModuleNames.AddRange(
+            new[]
+            {
+                "Core",
+                // ... add other public dependencies that you statically link with here ...
                 "NavigationSystem",
                 "AIModule",
                 "GameplayTasks"
             }
-			);
-			
-		
-		PrivateDependencyModuleNames.AddRange(
-			new string[]
-			{
-				"Projects",
-				"InputCore",
-				"UnrealEd",
-				"LevelEditor",
-				"CoreUObject",
-				"Engine",
-				"Slate",
-				"SlateCore",
-				// ... add private dependencies that you statically link with here ...	
-			}
-			);
-		
-		
-		DynamicallyLoadedModuleNames.AddRange(
-			new string[]
-			{
-				// ... add any modules that your module loads dynamically here ...
-			}
-			);
-	}
+        );
+
+
+        PrivateDependencyModuleNames.AddRange(
+            new[]
+            {
+                "Projects",
+                "InputCore",
+                "UnrealEd",
+                "LevelEditor",
+                "CoreUObject",
+                "Engine",
+                "Slate",
+                "SlateCore"
+                // ... add private dependencies that you statically link with here ...	
+            }
+        );
+
+
+        DynamicallyLoadedModuleNames.AddRange(
+            new string[]
+            {
+                // ... add any modules that your module loads dynamically here ...
+            }
+        );
+    }
 }
